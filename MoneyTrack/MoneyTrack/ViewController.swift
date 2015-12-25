bc34321701218e7f82007025abb47016970f0677
SrikantGithub <dora.jena@gmail.com>
2015-12-26 01:02:43 +0530

    Project added

---
+++ MoneyTrack/MoneyTrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoneyTrack
+//
+//  Created by Srikant kumar jena on 26/12/15.
+//  Copyright (c) 2015 Srikant kumar jena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MoneyTrack/MoneyTrackTests/MoneyTrackTests.swift
@@ -0,0 +1,36 @@
+//
+//  MoneyTrackTests.swift
+//  MoneyTrackTests
+//
+//  Created by Srikant kumar jena on 26/12/15.
+//  Copyright (c) 2015 Srikant kumar jena. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MoneyTrackTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
